90793856e8fc049257290fecc689e9703d3e81ce
Sean <94819913+ShangchenHsieh@users.noreply.github.com>
2026-08-24 14:04:45 -0700

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.py
@@ -0,0 +1,43 @@
+from typing import Optional
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def merge(arr: list[int], left: int, m: int, right: int):
+            left_arr, right_arr = arr[left:m+1], arr[m+1:right+1]
+            i, j, k = 0, 0, left
+
+            while i < len(left_arr) and j < len(right_arr): 
+                if left_arr[i] >= right_arr[j]: 
+                    arr[k] = right_arr[j]
+                    j += 1
+                else: 
+                    arr[k] = left_arr[i]
+                    i += 1
+
+                k += 1
+            while i < len(left_arr):
+                arr[k] = left_arr[i]
+                i += 1
+                k += 1
+
+            while j < len(right_arr):
+                arr[k] = right_arr[j]
+                j += 1
+                k += 1
+            
+
+        def mergeSort(array: list, left: int, right: int) -> Optional["list[int]"]: # divide and conquer
+            if left == right: 
+                return array
+            
+            m = (left + right) // 2
+            # left part 
+            mergeSort(array, left, m)
+            # right part 
+            mergeSort(array, m+1, right)
+            # and then merge at the end
+            merge(array, left, m, right)
+
+        mergeSort(nums, 0, len(nums)-1)
+
+        return nums
