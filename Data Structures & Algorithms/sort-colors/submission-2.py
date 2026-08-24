80f4d14f945ce5ce4ebe0eacd3052b9bd57047e0
Sean <94819913+ShangchenHsieh@users.noreply.github.com>
2026-08-24 14:20:07 -0700

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        
+        Noticed inputs are just 0, 1, 2
+        Intuition: counter
+        """
+        
+        counter = [0 for _ in range(3)]
+        
+        for n in nums: 
+            counter[n] += 1
+        
+        j = 0
+        for i in range(3): # iterate through the counter
+            while counter[i] != 0: # modify the number to the desire amount
+                nums[j] = i
+                j += 1
+                counter[i] -= 1 
